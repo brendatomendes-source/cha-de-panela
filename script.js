0c23e65912ecdd156e47f1178e4ec3d3e7139383
brendatomendes-source <brendatomendes@gmail.com>
2026-08-12 15:50:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// As funções do site serão adicionadas aqui depois.
+function copiarPix() {
+
+    const chave = document.getElementById("chave-pix").innerText;
+
+    navigator.clipboard.writeText(chave)
+        .then(function() {
+
+            const mensagem =
+                document.getElementById("mensagem-pix");
+
+            mensagem.style.display = "block";
+
+            setTimeout(function() {
+                mensagem.style.display = "none";
+            }, 3000);
+
+        });
+
+}
